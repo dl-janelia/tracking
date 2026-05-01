59d463e276335cef43cbd81035f9ee69a1c2991d
AlbertDominguez <albert.dominguezmantes@epfl.ch>
2026-05-01 16:00:57 +0200

    add transformers pt2draft

--- prepare_exercise.sh
+++ prepare_exercise.sh
@@ -1,11 +1,7 @@
 # Run black on .py files
 black solution$1.py
 
-# Convert .py to ipynb
-# "cell_metadata_filter": "all" preserve cell tags including our solution tags
-rm solution$1.ipynb
-jupytext --to ipynb --update-metadata '{"jupytext": {"cell_metadata_filter":"all"}}' solution$1.py
 
-# Create the exercise notebook by removing cell outputs and deleting cells tagged with "solution"
-# There is a bug in the nbconvert cli so we need to use the python API instead
-python convert-solution.py solution$1.ipynb exercise$1.ipynb
+jupytext --to ipynb --update-metadata '{"jupytext":{"cell_metadata_filter":"all"}}' solution$1.py
+jupyter nbconvert solution$1.ipynb --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_cell_tags solution --to notebook --output exercise$1.ipynb
+jupyter nbconvert solution$1.ipynb --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_cell_tags task --to notebook --output solution$1.ipynb
