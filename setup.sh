b647ecb11dbdf3b7a6d04581d5dbe31a19dae58b
Caroline Malin-Mayor <malinmayorc@janelia.hhmi.org>
2026-05-07 17:03:16 -0400

    Use uv in setup.sh

--- setup.sh
+++ setup.sh
@@ -9,24 +9,13 @@ then
 fi
 
 # Create environment
-conda create -y -n 08-tracking python=3.11
+uv sync
 
 # Activate environment
-conda activate 08-tracking
-
-# Install additional requirements
-if [[ "$CONDA_DEFAULT_ENV" == "08-tracking" ]]; then
-    echo "Environment activated successfully for package installs"
-    pip install numpy "motile>=0.3" "traccuracy>=0.1.1" "geff>=1.1.3" "trackastra" "motile-tracker>=4.6,<5" "funtracks>=1.8,<2" "zarr<3" matplotlib ipywidgets nbformat pandas ipykernel
-    python -m ipykernel install --user --name "08-tracking"
-else
-    echo "Failed to activate environment for package installs. Dependencies not installed!"
-fi
-
-conda deactivate
+source .venv/bin/activate
 
 # Download data from s3
-wget https://dl-at-mbl-data.s3.us-east-2.amazonaws.com/2025/09_tracking/data.zip
+wget https://dl-at-mbl-data.s3.us-east-2.amazonaws.com/2026/tracking/data.zip
 unzip data.zip
 rm data.zip
 
